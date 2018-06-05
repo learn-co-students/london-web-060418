3b361119421a22a8b868f962e3f80eeb579dff3f
rishter <rishikesh@flatironschool.com>
2018-06-05 17:50:23 +0100

    after lecture

--- 03_pigeon_organizer/nyc-pigeon-organizer/nyc_pigeon_organizer.rb
+++ 03_pigeon_organizer/nyc-pigeon-organizer/nyc_pigeon_organizer.rb
@@ -1,16 +1,40 @@
 require 'pry'
+#
+# def nyc_pigeon_organizer(data)
+#   # write your code here!
+#   final = {}
+#   data.each do |attribute_type, details|
+#     # puts "we would have hit a pry here with the attribute #{attribute}!"
+#     details.each do |attribute_name, names|
+#       names.each do |name|
+#         binding.pry
+#       end
+#     end
+#   end
+# end
+
+def add_color_to_pigeon(pigeon, color)
+end
+
+def add_pigeon_to_pigeon_list(pigeon_list, name)
+end
+
+# Layer 1 - Pigeon name
+  # layer 2 - attribute category
+    # layer 3 - atribute_value inside an array
 
 def nyc_pigeon_organizer(data)
-  # write your code here!
-  final = {}
-  data.each do |attribute_type, details|
-    # puts "we would have hit a pry here with the attribute #{attribute}!"
-    details.each do |attribute_name, names|
+  pigeon_list = {}
+  data.each do |attribute_category, attribute_values|
+    attribute_values.each do |attribute_value, names|
       names.each do |name|
-        binding.pry
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attribute_category] ||= []
+        pigeon_list[name][attribute_category] << attribute_value.to_s
       end
     end
   end
+  pigeon_list
 end
 
 pigeon_data = {
@@ -32,4 +56,5 @@ pigeon_data = {
   }
 }
 
+binding.pry
 nyc_pigeon_organizer(pigeon_data)
