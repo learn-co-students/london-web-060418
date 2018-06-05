150cedf8e5321aaadbd8e861b960c251c38fb36d
Rishikesh Tirumala <rishikesh@flatironschool.com>
2018-06-05 17:58:23 +0100

    Update nyc_pigeon_organizer.rb

--- 03_pigeon_organizer/nyc-pigeon-organizer/nyc_pigeon_organizer.rb
+++ 03_pigeon_organizer/nyc-pigeon-organizer/nyc_pigeon_organizer.rb
@@ -1,32 +1,15 @@
 require 'pry'
-#
-# def nyc_pigeon_organizer(data)
-#   # write your code here!
-#   final = {}
-#   data.each do |attribute_type, details|
-#     # puts "we would have hit a pry here with the attribute #{attribute}!"
-#     details.each do |attribute_name, names|
-#       names.each do |name|
-#         binding.pry
-#       end
-#     end
-#   end
-# end
-
-def add_color_to_pigeon(pigeon, color)
-end
-
-def add_pigeon_to_pigeon_list(pigeon_list, name)
-end
 
 # Layer 1 - Pigeon name
   # layer 2 - attribute category
     # layer 3 - atribute_value inside an array
 
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  data.each do |attribute_category, attribute_values|
-    attribute_values.each do |attribute_value, names|
+  # write your code here!
+  final = {}
+  data.each do |attribute_type, details|
+    # puts "we would have hit a pry here with the attribute #{attribute}!"
+    details.each do |attribute_name, names|
       names.each do |name|
         pigeon_list[name] ||= {}
         pigeon_list[name][attribute_category] ||= []
@@ -34,8 +17,10 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-  pigeon_list
+  final
 end
+
+
 
 pigeon_data = {
   :color => {
