45b22520c4249117ec135f2d445546b01dd46231
nikymorg <nicole.morgan@gmail.com>
2018-07-19 15:28:45 +0100

    post lecture

--- 26-oo-js/README.md
+++ 26-oo-js/README.md
@@ -3,27 +3,17 @@
 * Leverage Ruby's `self` to frame our JS `this` conversation
 * Recognize and value the differences
 * Answer Dan Abramov's [question](https://twitter.com/dan_abramov/status/790858537513656320)
-* When is the value of `this` set? When it's **NOT** set?
 * The JS environment rules that govern `this`
     1.  `this` within a function called with the keyword `new` in front will point to the newly created object
-    1.  `this` within a function called with apply/call/bind will be the object passed as the first parameter
+    <!-- 1.  `this` within a function called with apply/call/bind will be the object passed as the first parameter -->
     1.  `this` within a function called with a context (i.e. Object.method()) will be the object.
     1.  `this` for a simple function call (fn()) will be the window (browser) or the global object (Node). If we are in strict mode this will be undefined for simple function calls
 * How do we control `this`
 * This of arrow functions
 
 ## This vs Self
-- this refers to the instance of an object
-- fairly similar to self
-  - this tries to interpret the context more
-  - context of called versus defined
-
-- self is about context
-  - the receiver of the message or method
-  - what the method is being called on
-  - doesn't change
-- this is about where it is defined
-  - what object is receiving the function call
+- this and self are both about what is the receiver of the method call
+- in JS methods can be passed around as objects, to this can change
 
 ## Resources
 * [MDN `this` article](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this)
--- 26-oo-js/index.js
+++ 26-oo-js/index.js
@@ -2,6 +2,10 @@ function loggingIt() {
   return this
 }
 
+const logArrow = () => {
+  console.log(this)
+}
+
 function afterWork() {
   return `After 6pm I love to ${this.hobby}.`
 }
@@ -20,9 +24,6 @@ let brooke = {
 let steven = {
   role: 'TCF',
   hobby: 'ska band - trombone',
-  log: function loggingIt() {
-    return this
-  },
   afterWork: afterWork
 }
 
@@ -37,17 +38,87 @@ function peopleFactory(role, hobby, name) {
 
 // ES5
 // we can create a constructor function using the new keyword
+const Person = (function () {
+  // class variable
+  let all = []
+
+  let init = function(role, hobby, name) {
+    // instance variables
+    this.role = role
+    this.hobby = hobby
+    this.name = name
+    all.push(this)
+  }
+
+  // class method
+  init.all = function() {
+    return [...all]
+  }
+
+  return init
+})()
+
+Person.prototype.afterWork = afterWork
+
 function Person(role, hobby, name) {
   this.role = role
   this.hobby = hobby
   this.name = name
 }
 
-const dan = new Person('Lead', "skateboard", "Dan")
-
-// makes the function a property on the dan's prototype chain
+// makes the function a property on Person's prototype chain
 // all Person objects will utilize the same afterWork function in memory
-Person.prototype.afterWork = afterWork
-dan.afterWork() // "After 6pm I love to skateboard."
+
+// dan.afterWork() // "After 6pm I love to skateboard."
+
+const Person = (function() {
+  let all = []
+
+  return class Person {
+    constructor (role, hobby, name) {
+      // instance variables
+      this.role = role
+      this.hobby = hobby
+      this.name = name
+      all.push(this)
+    }
+
+    // class method
+    static all() {
+      return [...all]
+    }
+
+    // instance method
+    afterWork() {
+      return `After 6pm I love to ${this.hobby}.`
+    }
+  }
+})()
+
 
 // ES6
+class Person {
+  constructor (role, hobby, name) {
+    // instance variables
+    this.role = role
+    this.hobby = hobby
+    this.name = name
+    all.push(this)
+  }
+
+  // class method
+  static all() {
+    return [...all]
+  }
+
+  // instance method
+  afterWork() {
+    return `After 6pm I love to ${this.hobby}.`
+  }
+}
+
+
+
+
+
+
